c3d100f7037635ce5f04d662067936195d042256
Harshad Kataktalware <harshad0386@gmail.com>
2024-06-24 22:32:26 +0530

    test two

--- app/context/ToastContext.tsx
+++ app/context/ToastContext.tsx
@@ -4,7 +4,6 @@ import { Toaster } from "react-hot-toast";
 const ToasterContext = () => {
   return (
     <div>
-      <h1>this is for demo</h1>
       <Toaster position="top-center" reverseOrder={false} />
     </div>
   );

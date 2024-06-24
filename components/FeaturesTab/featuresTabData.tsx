3b363c6f2a5235a0ab08e7e1471e620941d3621b
Saurabh Dantani <62558826+SaurabhDantani@users.noreply.github.com>
2024-06-24 16:35:50 +0000

    update pages

--- components/FeaturesTab/featuresTabData.tsx
+++ components/FeaturesTab/featuresTabData.tsx
@@ -7,7 +7,7 @@ const featuresTabData: FeatureTab[] = [
     desc1: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ultricies lacus non fermentum ultrices. Fusce consectetur le.`,
     desc2: `    Nam id eleifend dui, id iaculis purus. Etiam lobortis neque nec finibus sagittis. Nulla ligula nunc egestas ut.`,
     image: "/images/features/features-light-01.png",
-    imageDark: "/images/features/features-dark-01.svg",
+    imageDark: "/images/features/house.jpg",
   },
   {
     id: "tabTwo",
--- components/FeaturesTab/index.tsx
+++ components/FeaturesTab/index.tsx
@@ -5,6 +5,7 @@ import FeaturesTabItem from "./FeaturesTabItem";
 import featuresTabData from "./featuresTabData";
 
 import { motion } from "framer-motion";
+import SectionHeader from "../Common/SectionHeader";
 
 const FeaturesTab = () => {
   const [currentTab, setCurrentTab] = useState("tabOne");
@@ -12,6 +13,13 @@ const FeaturesTab = () => {
   return (
     <>
       {/* <!-- ===== Features Tab Start ===== --> */}
+      <SectionHeader
+            headerInfo={{
+              title: `Commercial Spaces in Mumbai`,
+              subtitle: `Add some description for houses in mumbai using chat gpt or anything else.`,
+              description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor eros. Donec vitae tortor lacus. Phasellus aliquam ante in maximus.`,
+            }}
+          />
       <section className="relative pb-20 pt-18.5 lg:pb-22.5">
         <div className="relative mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
           <div className="absolute -top-16 -z-1 mx-auto h-[350px] w-[90%]">
